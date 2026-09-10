c652ecb19f4e4445fc4315afb5058c8cdc855a1f
Namrata3-patil <namratap3325@gmail.com>
2026-09-10 18:55:24 +0530

    Correct power operation print statement
    Fix formatting and add docstring for clarity.

---
+++ Hackerrank_Python/Core_Foundation/Power_operstion.py
@@ -0,0 +1,19 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+print(a**b +c**d)
+"""
+Print the result of  on one line.
+
+Sample Input
+
+9
+29
+7
+27
+Sample Output
+
+4710194409608608369201743232  
+"""
